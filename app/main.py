3496dd46c88948daf93fa4984789d892071c7500
Murillo Santos <113802902+tathuanan@users.noreply.github.com>
2025-11-30 18:41:31 -0300

    fix: adicionado CORS para requisições do frontend

--- app/main.py
+++ app/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.v1.api import api_router
 from app.core.settings import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version="1.0.0",
+)
+
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos os métodos (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"], # Permite todos os headers (Authorization, etc.)
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
